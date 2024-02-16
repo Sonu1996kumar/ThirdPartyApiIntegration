2aba31c8fe58fbb863fb440d4cc06633efd92a57
test <test@gmail.com>
2024-02-16 13:00:57 +0530

    3rd Party Integration

---
+++ src/main/java/com/twilioexample/TwitioexampleApplication.java
@@ -0,0 +1,13 @@
+package com.twilioexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitioexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitioexampleApplication.class, args);
+	}
+
+}
